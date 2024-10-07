55a2fd05ed0a581147885c5d9309c1c26d609fa3
Abi Turner <turner.abi@gmail.com>
2024-10-07 11:19:43 +0100

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Abigail Turner on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
